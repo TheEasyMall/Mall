ce24f97ff71f54cb18fd3dfbaa49b14732eb65d4
Chuong Huynh <tanchuonghuynh3@gmail.com>
2025-03-02 15:29:33 +0700

    Set up MVC

---
+++ Mall/Mall.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mall.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
